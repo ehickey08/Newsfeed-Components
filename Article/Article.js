61222d772f980c07e047c640fa7e6287168096fc
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-05-15 16:13:17 -0400

    Stop propagatin on expand/close article buttons

--- Article/Article.js
+++ Article/Article.js
@@ -7,16 +7,18 @@ class Article {
     // create a reference to the ".expandButton" class. 
     this.expandButton = this.article.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand';
+    this.expandButton.textContent = 'Click to Expand';
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     
-    this.expandButton.addEventListener('click', () => this.expandArticle());
+    this.expandButton.addEventListener('click', () => this.expandArticle(event));
   }
 
-  expandArticle() {
+  expandArticle(event) {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.article.classList.toggle('article-open');
-    this.expandButton.textContent = (this.expandButton.textContent === 'expand') ? 'close' : 'expand';
+    this.expandButton.textContent = (this.expandButton.textContent === 'Click to Expand') ? 'Click to Close' : 'Click to Expand';
+    console.log(event);
+    event.stopPropagation();
   }
 }
 
